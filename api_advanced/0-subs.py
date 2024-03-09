4482019b541c6badda4c3bebb7eebfe70033686e
ngabo-dev <j.niyongabo@alustudent.com>
2024-03-09 14:20:19 -0800

    task 0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,8 +5,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """DOC"""
-    reddit_url = "https://www.reddit.com/r/{subreddit}/about.json"
-        .format(subreddit)
+    reddit_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     header = {'User-agent': 'Mozilla/5.0'}
     response = requests.get(reddit_url,
